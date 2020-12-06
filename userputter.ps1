b9157299a542d3da97f211bd84b5576b25040cc4
Peter Hauck <peter@pggb.net>
2020-12-06 00:50:41 +0000

    update to path

--- userputter.ps1
+++ userputter.ps1
@@ -4,7 +4,7 @@ $app_role_name = "User"
 $sp = Get-AzureADServicePrincipal -Filter "displayName eq '$app_name'"
 $appRole = $sp.AppRoles | Where-Object { $_.DisplayName -eq $app_role_name }
 
-foreach($username in Get-Content -Path c:\data\users.txt) {
+foreach($username in Get-Content -Path .\Users.txt) {
     $user = Get-AzureADUser -ObjectId "$username"
     Write-Output $user
 }
