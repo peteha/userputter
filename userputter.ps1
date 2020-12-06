c79dfbcc04e6dd118fd7a7ab85d58865765c7461
Peter Hauck <peter@pggb.net>
2020-12-06 10:44:41 +1000

    Initial

---
+++ userputter.ps1
@@ -0,0 +1,10 @@
+#User_Putter
+$app_name = "Cisco Webex"
+$app_role_name = "User"
+$sp = Get-AzureADServicePrincipal -Filter "displayName eq '$app_name'"
+$appRole = $sp.AppRoles | Where-Object { $_.DisplayName -eq $app_role_name }
+
+foreach($username in Get-Content -Path c:\data\users.txt) {
+    $user = Get-AzureADUser -ObjectId "$username"
+    Write-Output $user
+}
